ab7f25a2871f7f86edb30ef356e6e38da27351eb
07Ritik <128927956+07Ritik@users.noreply.github.com>
2023-09-30 17:33:08 +0000

    conversion of data

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,49 @@
+    // let score="33"//output Number
+
+    // console.log(typeof (score));
+    // //let score="33";//output string
+    // console.log(typeof score);
+    // let valueInNumber=Number(score)//it conert anyvalue like if value type is string then also it convert into Number
+    // console.log(typeof valueInNumber)
+
+//####################################Operation####################################################################
+
+let value=3;
+let NegValue = -value;
+//console.log(NegValue); output => value will convert in negative
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+// let str1 = "hello";
+// let str2 = "Ritik"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("2" + 2);// o/p =>22
+// console.log(2 + "2");// o/p => 22
+// console.log("2" + 2 +1);//o/p => 221
+// console.log(2 + 2 +"1");//o/p => 41
+
+
+
+// for better understand must visithis web 
+// https://262.ecma-international.org/13.0/#sec-abstract-operation
+
+
+let GameCounter = 100;
+++GameCounter;
+console.log(GameCounter);
+
+// for prefix and postfix visit this web
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+

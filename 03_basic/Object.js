16a7958c18f82e608d5783268e9eea4c8fafb7af
07Ritik <128927956+07Ritik@users.noreply.github.com>
2023-10-16 14:34:22 +0000

    Objects

---
+++ 03_basic/Object.js
@@ -0,0 +1,18 @@
+// object 
+const Ritik={
+    Address:"Uttar Prsdesh",
+    Age:22,
+    Email:"ritik@gmail.com",
+    Name:"Ritik",
+}
+
+//console.log(Ritik["Email"]);
+console.log(Ritik);
+//var Ritik = {};
+
+Ritik.greeting = function() {
+    console.log("Hello its Shukla's world");
+}
+//console.log(Ritik.greeting());
+//console.log(`Hello its Shukla's world , ${this.Name}`);
+
